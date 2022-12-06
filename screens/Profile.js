eb2c23366b9bd058acdc423dcc078980b6cdc416
leonardoaranhabyjus <leonardo.aranha@byjusfutureschool.com>
2022-12-06 14:33:20 -0300

    init

---
+++ screens/Profile.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync();
+      return (
+        <View style={styles.container}>
+          <Text>Profile</Text>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
